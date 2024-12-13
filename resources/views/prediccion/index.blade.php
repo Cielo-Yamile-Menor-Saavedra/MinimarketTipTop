2764b80fd5a740bd3859539e23e7d8890cd11efc
IRVIN69 <g333300120@unitru.edu.pe>
2024-12-12 23:32:25 -0500

    prueba

--- resources/views/prediccion/index.blade.php
+++ resources/views/prediccion/index.blade.php
@@ -1,6 +1,14 @@
-{{-- @extends('layout.plantilla')
+@extends('layout.plantilla')
 @section('contenido')
 <a href="https://prediccion-ventas.streamlit.app" target="_blank" class="btn btn-primary">
     Abrir predicción de ventas
 </a>
+@endsection
+
+
+{{-- @extends('layout.plantilla')
+@section('contenido')
+<iframe src="http://localhost:8501" width="100%" height="600" frameborder="0"></iframe>
 @endsection --}}
+
+
